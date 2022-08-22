637d382df9469764be05afa5ee8fa94ab8ab9ec8
Trần Tuấn Đạt <76252195+trantuandatvietnam@users.noreply.github.com>
2022-08-22 10:04:02 +0700

    REDUX TOOLKIT NEW BRAND

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducer";
 
 const composeEnhancers = composeWithDevTools();
-
+// REDUX TOOLKIT
 const store = createStore(rootReducer, composeEnhancers); // Phải có dòng này thì dev tool trên trình duyệt mới có thể sử dụng được
 
 export default store;
