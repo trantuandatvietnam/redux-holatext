cfe138f728e3d6e9429811c04ee0986ee0a67cbe
Trần Tuấn Đạt <76252195+trantuandatvietnam@users.noreply.github.com>
2022-10-07 09:36:32 +0700

    Delete reducer.js

--- src/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import filtersReducer from "../components/Filters/FiltersSlice";
-import todoListReducer from "../components/Todo/TodoSlice";
-
-const rootReducer = combineReducers({
-  filters: filtersReducer,
-  todoList: todoListReducer,
-});
-export default rootReducer;
